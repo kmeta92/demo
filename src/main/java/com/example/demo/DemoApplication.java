855fb5a2825e2671ea6c64a6475e56ba0f1df8e2
Java-Advanced <emilyohanov@yahoo.com>
2019-10-28 20:56:07 +0200

    1st commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
+		String string
+
 		System.out.println("Hello world!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
